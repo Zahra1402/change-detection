b8158fd2cd458698ef0a155e78a4846f2ee38415
Zahra Azarm <132208503+Zahra1402@users.noreply.github.com>
2023-06-14 09:37:34 +0330

    Update cloudshadow_ndvi.py

--- cloudshadow_ndvi.py
+++ cloudshadow_ndvi.py
@@ -5,27 +5,21 @@ import matplotlib.pyplot as plt
 
 
 def cloudshadow(image1):
-    # _____ creates ndvi image
-    B4 = image1[:, :, 3]
-    B8 = image1[:, :, 7]
-    ndvi = (B8 - B4) / (B8 + B4)
-    # _____ creates ndsi image
-    swir1   = image1[:, :,11]
-    green   = image1[:, :, 2]
-    ndsi = (green - swir1) / (green + swir1)
+    b1   = image1[:, :,11]
+    b2  = image1[:, :, 2]
+    b3   = image1[:, :, 5]
 
-    #Threshold the NDSI and NDVI to identify cloud and shadow pixels
-    cloud_mask  = (ndsi < 0.95) & (ndvi <  0.3)
-    shadow_mask = (ndsi > 0.92) & (ndvi <  0.1)
+    ratiob = (b2 - b1) / (b2 + b1)
+    # _____ creates ndsi image
+    ndsi = (b3 - b1) / (b3 + b1)
+    #Threshold the NDSI and ratiob to identify cloud and shadow pixels
+    cloud_mask  = (ndsi>0.95) & (ratiob > 0.93)
+    shadow_mask = (ndsi>0.92) & (ratiob >  0.93)
 
     #Combine the cloud and shadow masks
     mask = ~(cloud_mask | shadow_mask)
-   
+       
     # Apply the mask to the original image
     cldshw1 = image1 * mask[..., np.newaxis]
 
-    return cldshw1
-
-
-
-
+    return cldshw1, ndsi , ratiob
